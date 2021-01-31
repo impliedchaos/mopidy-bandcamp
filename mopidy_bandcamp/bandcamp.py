d5a014032aae5681fe7cd64ea16a505bf2bc7d65
impliedchaos <crap@dharmadevil.com>
2021-01-31 08:53:06 -0500

    error handling

--- mopidy_bandcamp/bandcamp.py
+++ mopidy_bandcamp/bandcamp.py
@@ -65,10 +65,7 @@ class BandcampClient:
 
     def search(self, query):
         resp = requests.get(f"{self.BASE_URL}/fuzzysearch/1/app_autocomplete?q={query}")
-        json = resp.json()
-        if 'error' in json:
-            raise RuntimeError(json['error_message'])
-        return json
+        return resp.json()
 
     def discover(self, sort="top", genre="all", tag=None, page=0):
         query = f"{self.BASE_URL}/discover/2/get?s={sort}&l=0&emulate_loc=true&f=all&g={genre}"
@@ -77,7 +74,4 @@ class BandcampClient:
         if page > 0:
             query += f"&p={page}"
         resp = requests.get(query)
-        json = resp.json()
-        if 'error' in json:
-            raise RuntimeError(json['error_message'])
-        return json
+        return resp.json()
