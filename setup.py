856630886c71a443f94e8b01201e6b36a76827b4
impliedchaos <crap@dharmadevil.com>
2021-02-02 08:36:06 -0500

    python versions

--- setup.py
+++ setup.py
@@ -37,6 +37,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Multimedia :: Sound/Audio :: Players",
     ],
 )
